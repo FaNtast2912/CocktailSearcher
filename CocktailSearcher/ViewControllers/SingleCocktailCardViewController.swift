1002650c4463478b21672d12616f5685d5b77642
FaNtast2912 <z_maks@inbox.ru>
2024-09-24 14:13:20 +0300

    app scheme added

---
+++ CocktailSearcher/ViewControllers/SingleCocktailCardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SingleCocktailCardViewController.swift
+//  CocktailSearcher
+//
+//  Created by Maksim Zakharov on 24.09.2024.
+//
+
+import Foundation
+import UIKit
+
+class SingleCocktailCardViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+
+    // MARK: - Public Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
+
+    // MARK: - Overrides Methods
+
+    // MARK: - IB Actions
+
+    // MARK: - Public Methods
+
+    // MARK: - Private Methods
+    
+}
